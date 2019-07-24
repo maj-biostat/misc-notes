e9353b960507b928fb82b58b3dc51fab2d01ba74
Mark Jones <mark.jones@telethonkids.org.au>
2019-07-24 13:07:07 +0800

    Install R package script

---
+++ install_R_pkgs.R
@@ -0,0 +1,60 @@
+install.packages("knitr", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("kableExtra", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("configr", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("futile.logger", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("survival", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("inline", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("doParallel", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("foreach", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("truncnorm", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("beepr", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("tinytex", dependencies = T, repos = "https://cran.curtin.edu.au")
+
+
+install.packages("microbenchmark", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("coin", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("devtools", dependencies = T, repos = "https://cran.curtin.edu.au")
+devtools::install_github("rmcelreath/rethinking")
+
+install.packages("eph", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("pch", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("nphsim", dependencies = T, repos = "https://cran.curtin.edu.au")
+
+
+# Stepped wedge sample size:
+# https://github.com/giabaio/SWSamp
+
+install.packages("SWSamp",
+	repos=c("http://www.statistica.it/gianluca/R",
+		"https://cran.rstudio.org",
+		"https://inla.r-inla-download.org/R/stable"),
+	dependencies=TRUE
+)
+
+# Stan follow instructions
+# https://github.com/stan-dev/rstan/wiki/RStan-Getting-Started
+
+install.packages("rstan", dependencies = TRUE, repos = "https://cran.curtin.edu.au")
+install.packages("brms", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("coda", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("loo", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("rjags", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("pwr", dependencies = T, repos = "https://cran.curtin.edu.au")
+
+install.packages("mcmc", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("ggfortify", dependencies = T, repos = "https://cran.curtin.edu.au")
+
+install.packages("LaplacesDemon", dependencies = T, repos = "https://cran.curtin.edu.au")
+install.packages("invgamma", dependencies = T, repos = "https://cran.curtin.edu.au")
+
+install.packages(c("HRW"),dependencies=TRUE, repos = "https://cran.curtin.edu.au")
+install.packages(c("Ecdat"),dependencies=TRUE, repos = "https://cran.curtin.edu.au")
+install.packages(c("mlbench"),dependencies=TRUE, repos = "https://cran.curtin.edu.au")
+install.packages(c("quantreg"),dependencies=TRUE, repos = "https://cran.curtin.edu.au")
+install.packages(c("gam"),dependencies=TRUE, repos = "https://cran.curtin.edu.au")
+install.packages(c("gamlss"),dependencies=TRUE, repos = "https://cran.curtin.edu.au")
+
+
+
+
+tinytex::install_tinytex(T)
