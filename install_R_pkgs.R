53303d2bf90a78a5287acab8ed82f3e068ed1aa8
Mark Jones <mark.jones@telethonkids.org.au>
2019-07-25 10:43:39 +0800

    Refined install script

--- install_R_pkgs.R
+++ install_R_pkgs.R
@@ -1,85 +1,77 @@
-install.packages("ggplot2", dependencies = T, repos = 'https://cran.curtin.edu.au')
-install.packages("tidyr", dependencies = T, repos = 'https://cran.curtin.edu.au')
-install.packages("lintr", dependencies = T, repos = 'https://cran.curtin.edu.au')
-install.packages("dplyr", dependencies = T, repos = 'https://cran.curtin.edu.au')
-install.packages("pryr", dependencies = T, repos = 'https://cran.curtin.edu.au')
-install.packages("Rcpp", dependencies = T, repos = 'https://cran.curtin.edu.au')
-install.packages("data.table", dependencies = T, repos = 'https://cran.curtin.edu.au')
-install.packages("RcppEigen", dependencies = T, repos = 'https://cran.curtin.edu.au')
-install.packages("RcppArmadillo", dependencies = T, repos = 'https://cran.curtin.edu.au')
-install.packages("RcppDist", dependencies = T, repos = 'https://cran.curtin.edu.au')
-install.packages("msm", dependencies = T, repos = 'https://cran.curtin.edu.au')
-install.packages("grid", dependencies = T, repos = 'https://cran.curtin.edu.au')
-install.packages("simstudy", dependencies = T, repos = 'https://cran.curtin.edu.au')
-install.packages("gridExtra", dependencies = T, repos = 'https://cran.curtin.edu.au')
-install.packages("optparse", dependencies = T, repos = 'https://cran.curtin.edu.au')
-install.packages("formattable", dependencies = T, repos = 'https://cran.curtin.edu.au')
-install.packages("psych", dependencies = T, repos = 'https://cran.curtin.edu.au')
-install.packages("RcppParallel", dependencies = T, repos = 'https://cran.curtin.edu.au')
+
+dotinytex <- FALSE
+
+if(dotinytex){
+  message("Installing packages and Tex install.")
+} else {
+  message("Installing packages, NO Tex install.")
+}
+
+message("")
+
+check_for_pkg <- function(x){
+  as.character(x) %in% rownames(installed.packages())
+}
+
+# do this first otherwise get a load error
+if(!check_for_pkg("remotes")){
+  install.packages("remotes", dependencies = T, repos = 'https://cran.curtin.edu.au')
+  message("Installed remotes, now exiting, restart to complete installations.")
+  quit()
+}
+
+if(!check_for_pkg("SWSamp")){
+  remotes::install_github("rmcelreath/rethinking")
+}
+
+pkgs <- c("beepr", "bookdown", "brms", "coda", "coin", "configr", "data.table",
+  "devtools", "doParallel", "dplyr", "Ecdat", # "eph",
+  "foreach",
+  "formattable", "futile.logger", "futile.logger", "gam", "gamlss",
+  "ggfortify", "grid", "gridExtra", "HRW", "inline", "invgamma",
+  "kableExtra", "knitr", "LaplacesDemon", "lintr", "loo", "lubridate",
+  "mcmc", "microbenchmark", "mlbench", "msm", "mvtnorm", "nphsim", "optparse",
+  "pch", "pryr", "psych", "pwr", "quantreg",
+  "remotes",
+  "Rcpp", "RcppArmadillo",
+  "RcppDist", "RcppEigen", "RcppParallel", "rjags", "rmarkdown",
+  "rstan", "simstudy", "survival", "tibble", "tidyr", "tinytex",
+  "truncnorm", "tryCatchLog")
+
+for(i in 1:length(pkgs)){
+  if(!check_for_pkg(pkgs[i])){
+
+    message(paste0("Installing    : ", pkgs[i]))
+
+    install.packages(pkgs[i], dependencies = T, repos = 'https://cran.curtin.edu.au')
+  }
+}
 
 
-install.packages("knitr", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("kableExtra", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("configr", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("futile.logger", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("survival", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("inline", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("doParallel", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("foreach", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("truncnorm", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("beepr", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("tinytex", dependencies = T, repos = "https://cran.curtin.edu.au")
 
-
-install.packages("microbenchmark", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("coin", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("devtools", dependencies = T, repos = "https://cran.curtin.edu.au")
-devtools::install_github("rmcelreath/rethinking")
-
-install.packages("eph", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("pch", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("nphsim", dependencies = T, repos = "https://cran.curtin.edu.au")
-
-
-# Stepped wedge sample size:
-# https://github.com/giabaio/SWSamp
-
-install.packages("SWSamp",
+if(!check_for_pkg("SWSamp")){
+  install.packages("SWSamp",
 	repos=c("http://www.statistica.it/gianluca/R",
 		"https://cran.rstudio.org",
 		"https://inla.r-inla-download.org/R/stable"),
 	dependencies=TRUE
-)
+  )
+}
+if(!check_for_pkg("nphsim")){
+  remotes::install_github("keaven/nphsim")
+}
 
-# Stan follow instructions
-# https://github.com/stan-dev/rstan/wiki/RStan-Getting-Started
+pkgs <- c(pkgs,
+  "SWSamp", "rethinking", "nphsim")
 
-install.packages("rstan", dependencies = TRUE, repos = "https://cran.curtin.edu.au")
-install.packages("brms", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("coda", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("loo", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("rjags", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("pwr", dependencies = T, repos = "https://cran.curtin.edu.au")
+for(i in 1:length(pkgs)){
+  if(check_for_pkg(pkgs[i])){
+    message(paste0("Installed     : ", pkgs[i]))
+  } else {
+    message(paste0("NOT installed : ", pkgs[i]))
+  }
+}
 
-install.packages("mcmc", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("ggfortify", dependencies = T, repos = "https://cran.curtin.edu.au")
-
-install.packages("LaplacesDemon", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("invgamma", dependencies = T, repos = "https://cran.curtin.edu.au")
-
-install.packages(c("HRW"),dependencies=TRUE, repos = "https://cran.curtin.edu.au")
-install.packages(c("Ecdat"),dependencies=TRUE, repos = "https://cran.curtin.edu.au")
-install.packages(c("mlbench"),dependencies=TRUE, repos = "https://cran.curtin.edu.au")
-install.packages(c("quantreg"),dependencies=TRUE, repos = "https://cran.curtin.edu.au")
-install.packages(c("gam"),dependencies=TRUE, repos = "https://cran.curtin.edu.au")
-install.packages(c("gamlss"),dependencies=TRUE, repos = "https://cran.curtin.edu.au")
-
-
-install.packages("tibble", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("futile.logger", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("tryCatchLog", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("bookdown", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("rmarkdown", dependencies = T, repos = "https://cran.curtin.edu.au")
-install.packages("lubridate", dependencies = T, repos = "https://cran.curtin.edu.au")
-
-tinytex::install_tinytex(T)
+if(dotinytex){
+  tinytex::install_tinytex(T)
+}
---
+++ run.sh
@@ -0,0 +1,4 @@
+
+
+/usr/bin/Rscript --no-save --no-restore --verbose install_R_pkgs.R
+
